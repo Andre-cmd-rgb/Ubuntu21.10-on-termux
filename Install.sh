00fbecc566be6611143014aa060013affbcb4420
Andre-cmd-rgb <85292605+Andre-cmd-rgb@users.noreply.github.com>
2021-11-05 17:48:48 +0100

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -1,12 +1,18 @@
 #!/usr/bin/env bash
+# set up colors
 RST="\e[0m"
-RED="\e[1;31m" # *This is bold 
+RED="\e[1;31m" # *This is bold
 GREEN="\e[1;32m"
 BLUE="\e[34m"
 DC=${BLUE}
+# version
 version="01"
-version_code_name="installer0.1"
-installer_authors="Andre-Cmd-Rgb"
-apt update && sudo apt upgrade
-pause 1
-printf "\e[1;32m Xfce installer v${version} ${version_code_name} \n by ${installer_authors}"
+version_code_name="Installer0.1"
+installer_authors="Andre"
+
+# update system
+apt update && apt upgrade
+# wait
+sleep 1
+# print
+printf "\e[1;32m Xfce installer v${version} ${version_code_name} \n"
