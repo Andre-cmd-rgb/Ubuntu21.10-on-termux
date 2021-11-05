47c2b0c27ef30114c32ac81d7e1aceb85cd70457
Andre-cmd-rgb <85292605+Andre-cmd-rgb@users.noreply.github.com>
2021-11-05 17:34:01 +0100

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -1,6 +1,6 @@
 version="01"
 version_code_name="installer0.1"
 installer_authors="Andre-Cmd-Rgb"
-sudo apt update && sudo apt upgrade
+apt update && sudo apt upgrade
 pause 1
 shout "\e[1;32m Udroid Installer v${version} ${version_code_name} \n by ${installer_authors}"
