17cc08cb1968a62a7607892aa7cd91b188e8a382
Andre-cmd-rgb <85292605+Andre-cmd-rgb@users.noreply.github.com>
2021-11-05 18:24:24 +0100

    Update update.sh

--- update.sh
+++ update.sh
@@ -2,7 +2,8 @@
 apt update && apt upgrade && apt install wget && apt install git
 # cloning
 https://raw.githubusercontent.com/Andre-cmd-rgb/test-termux/main/Install.sh
+https://raw.githubusercontent.com/Andre-cmd-rgb/test-termux/main/update.sh
 # 
 chmod +x Install.sh
 # launching
-./Install.sh
+./update.sh
