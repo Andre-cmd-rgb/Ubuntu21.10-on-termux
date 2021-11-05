75efa1dce8fa0471dcbc8f147dd27d5348476b2a
Andre-cmd-rgb <85292605+Andre-cmd-rgb@users.noreply.github.com>
2021-11-05 17:51:11 +0100

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,8 @@
+# updating system
+apt update && apt upgrade && apt install wget && apt install git
+# cloning
+wget https://raw.githubusercontent.com/Andre-cmd-rgb/test-termux/main/Install.sh
+# 
+chmod +x Install.sh
+# launching
+./Install.sh
