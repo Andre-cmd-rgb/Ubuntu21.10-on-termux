694d6cfd46d8c33c1ff598e8f0b9d9718369743f
Andre-cmd-rgb <85292605+Andre-cmd-rgb@users.noreply.github.com>
2021-11-05 18:44:18 +0100

    Update update.sh

--- update.sh
+++ update.sh
@@ -6,6 +6,10 @@ wget https://raw.githubusercontent.com/Andre-cmd-rgb/test-termux/main/update.sh
 # 
 chmod +x Install.sh
 chmod +x update.sh
+chmod +x update.sh.1
+chmod +x update.sh.2
+chmod +x update.sh.3
+chmod +x update.sh.4
 # clear
 clear
 rm update.sh
