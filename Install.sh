0a0f406c389338324194ef0dc807ff2c769988c6
Andre-cmd-rgb <85292605+Andre-cmd-rgb@users.noreply.github.com>
2021-11-05 17:40:10 +0100

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -9,4 +9,4 @@ version_code_name="installer0.1"
 installer_authors="Andre-Cmd-Rgb"
 apt update && sudo apt upgrade
 pause 1
-shout "\e[1;32m Udroid Installer v${version} ${version_code_name} \n by ${installer_authors}"
+printf "\e[1;32m Xfce installer v${version} ${version_code_name} \n by ${installer_authors}"
