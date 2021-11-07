23b3ec5ae5206b8de584c08b035063cccc87bdc8
Andre-cmd-rgb <85292605+Andre-cmd-rgb@users.noreply.github.com>
2021-11-07 08:23:47 +0100

    Update Ubuntu21.04.sh

--- Ubuntu21.04.sh
+++ Ubuntu21.04.sh
@@ -9,3 +9,8 @@ TARBALL_URL['arm']="https://github.com/termux/proot-distro/releases/download/v2.
 TARBALL_SHA256['arm']="541415c3475bf1e15a1a4e91e2b1291410ed63a1a4b6d403f9096754d8f2bd74"
 TARBALL_URL['x86_64']="https://github.com/termux/proot-distro/releases/download/v2.3.1/ubuntu-x86_64-pd-v2.3.1.tar.xz"
 TARBALL_SHA256['x86_64']="c728976dcc66eed5ab4cb550c96b9d3169f7a46dd56736732b5eba1c48b6c58e"
+
+distro_setup() {
+	run_proot_cmd apt update
+	run_proot_cmd apt upgrade 
+}
