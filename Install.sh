45105db70af53ae840cf85d83480b3396a9d0bfc
Andre-cmd-rgb <85292605+Andre-cmd-rgb@users.noreply.github.com>
2021-11-07 07:34:44 +0100

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 # set up colors
 RST="\e[0m"
 RED="\e[1;31m" # *This is bold
@@ -16,3 +16,4 @@ apt update && apt upgrade
 sleep 1
 # print
 printf "\e[1;32m Xfce installer v${version} ${version_code_name} \n"
+
