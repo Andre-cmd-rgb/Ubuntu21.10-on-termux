4473f6e23f9e5b722d7cc0d365c5a4921e240a5a
Andre-cmd-rgb <85292605+Andre-cmd-rgb@users.noreply.github.com>
2021-11-16 13:55:32 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 cd /data/data/com.termux/files/usr/etc/proot-distro/
 wget https://raw.githubusercontent.com/Andre-cmd-rgb/test-termux/main/Ubuntu21.04.sh
-wget https://raw.githubusercontent.com/Andre-cmd-rgb/test-termux/main/alpine.sh
+wget https://raw.githubusercontent.com/Andre-cmd-rgb/test-termux/main/alpine_kde.sh
