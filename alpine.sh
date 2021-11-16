10914133d38414e626dc66c6ef73e3b9854755d4
Andre-cmd-rgb <85292605+Andre-cmd-rgb@users.noreply.github.com>
2021-11-16 13:48:11 +0100

    Update alpine.sh

--- alpine.sh
+++ alpine.sh
@@ -1,1 +1,7 @@
+DISTRO_NAME="alpine"
+DISTRO_COMMENT="alpine kde"
 
+TARBALL_STRIP_OPT=0
+
+TARBALL_URL['x86_64']="https://github.com/Andre-cmd-rgb/test-termux/releases/download/0.1/alpine-x86_64-pd-v2.5.0.tar.xz"
+TARBALL_SHA256['x86_64']="80097be9cfedcf8f1d9128968356e8c14e7ab15abed51ca6936be69025db694c"
