d52dacdc49dd0da2a84d60740729637632e3539a
Andre-cmd-rgb <85292605+Andre-cmd-rgb@users.noreply.github.com>
2022-02-07 08:55:36 +0100

    Update Ubuntu21.10.sh

--- Ubuntu21.10.sh
+++ Ubuntu21.10.sh
@@ -4,7 +4,7 @@ DISTRO_COMMENT="Ubuntu 21.10 xfce"
 TARBALL_STRIP_OPT=0
 
 TARBALL_URL['aarch64']="https://github.com/Andre-cmd-rgb/Ubuntu21.10-on-termux/releases/download/0.2/ubuntu-aarch64-pd-v2.8.0.tar.xz"
-TARBALL_SHA256['aarch64']="599a0af87b110a9eab9f6f84b43243e497a73403397aeddb0d0b3cdb4ea54aa6"
+TARBALL_SHA256['aarch64']="e0bb4a06215d39bb8ff3a4469bb69dd78babc39dad9c82cd01e4ac52eba8c7b6"
 TARBALL_URL['arm']="https://github.com/Andre-cmd-rgb/Ubuntu21.10-on-termux/releases/download/0.2/ubuntu-arm-pd-v2.8.0.tar.xz"
 TARBALL_SHA256['arm']="541415c3475bf1e15a1a4e91e2b1291410ed63a1a4b6d403f9096754d8f2bd74"
 TARBALL_URL['x86_64']="https://github.com/Andre-cmd-rgb/Ubuntu21.10-on-termux/releases/download/0.2/ubuntu-x86_64-pd-v2.8.0.tar.xz"
