8b16095be0518c49d57eaebf566860218befd29f
Andre-cmd-rgb <85292605+Andre-cmd-rgb@users.noreply.github.com>
2022-02-07 09:00:38 +0100

    Updated to Ubuntu21.10

--- Ubuntu21.10.sh
+++ Ubuntu21.10.sh
@@ -8,4 +8,4 @@ TARBALL_SHA256['aarch64']="e0bb4a06215d39bb8ff3a4469bb69dd78babc39dad9c82cd01e4a
 TARBALL_URL['arm']="https://github.com/Andre-cmd-rgb/Ubuntu21.10-on-termux/releases/download/0.2/ubuntu-arm-pd-v2.8.0.tar.xz"
 TARBALL_SHA256['arm']="e985999097c3dd8692342867c6184b1a922dfee78a575ef25747da98b964a8c9"
 TARBALL_URL['x86_64']="https://github.com/Andre-cmd-rgb/Ubuntu21.10-on-termux/releases/download/0.2/ubuntu-x86_64-pd-v2.8.0.tar.xz"
-TARBALL_SHA256['x86_64']="c728976dcc66eed5ab4cb550c96b9d3169f7a46dd56736732b5eba1c48b6c58e"
+TARBALL_SHA256['x86_64']="0c8c19908df6eeae2d963fb00e1153c982b81386a480c16bfe683dfcdea404ec"
