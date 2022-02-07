52314f0d92213d4dd47bd8ed66ce97750b4ab5dd
Andre-cmd-rgb <85292605+Andre-cmd-rgb@users.noreply.github.com>
2022-02-07 09:03:06 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,2 @@
 cd /data/data/com.termux/files/usr/etc/proot-distro/
-wget https://raw.githubusercontent.com/Andre-cmd-rgb/Ubuntu21.10-on-termux/main/install.sh
+wget https://raw.githubusercontent.com/Andre-cmd-rgb/Ubuntu21.10-on-termux/main/Ubuntu21.10.sh
